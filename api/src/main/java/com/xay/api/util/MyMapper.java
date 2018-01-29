6d14359d25df36f03913fd9d20fcb063a2c83245
xiaogc123 <xiaogc_a@163.com>
2018-01-29 22:49:16 +0800

    集成Generator插件

---
+++ api/src/main/java/com/xay/api/util/MyMapper.java
@@ -0,0 +1,11 @@
+package com.xay.api.util;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 继承自己的MyMapper
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+}
